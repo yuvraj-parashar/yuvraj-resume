73241c808250a76e90089a88f1e97ce0c501df32
Yuvraj Parashar <yuvrajparashar710@gmail.com>
2021-05-26 18:49:16 +0530

    Updates

---
+++ precache-manifest.d8ebb814385533c996e6d3e2f1860e4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f80f6eb745713ce99a42",
+    "url": "/yuvraj-resume/static/css/main.3a1284b2.chunk.css"
+  },
+  {
+    "revision": "f80f6eb745713ce99a42",
+    "url": "/yuvraj-resume/static/js/main.f80f6eb7.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/yuvraj-resume/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "b24e1574b135833bb3e0",
+    "url": "/yuvraj-resume/static/js/runtime~main.b24e1574.js"
+  },
+  {
+    "revision": "0b1ca1ea3d8998683c8f1fef804ca6eb",
+    "url": "/yuvraj-resume/index.html"
+  }
+];
