f7a0ac68b1a28393c5be7b9e8bf0d57fa72d38b4
Yuvraj Parashar <yuvrajparashar710@gmail.com>
2021-05-26 18:36:24 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+export default class About extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="about">
+         <div className="row">
+
+            <div className="three columns">
+
+               <img className="profile-pic"  src="https://media-exp1.licdn.com/dms/image/C4E03AQEVJKkkgziNgQ/profile-displayphoto-shrink_800_800/0/1605250707052?e=1627516800&v=beta&t=1Fxy2aYlxORrEHhIW_il2lH44tcwPUiSYawUGsFBmJ0" alt="" />
+
+            </div>
+
+            <div className="nine columns main-col">
+
+               <h2>About Me</h2>
+               <p>
+               {
+                 resumeData.aboutme
+               }
+               </p>
+
+               <div className="row">
+
+                  <div className="columns contact-details">
+
+                  <h2>Contact Details</h2>
+                  <p className="address">
+       						<span>{resumeData.name}</span>
+                     <br></br>
+       						   <span>
+                     {resumeData.address}
+                    </span>
+                    <br></br>
+                    <span>{resumeData.website}</span>
+       					   </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+    );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,115 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/yuvraj-parashar",
+    "name": "Yuvraj Parashar",
+    "role": "Web Developer",
+    "linkedinId":"https://www.linkedin.com/in/yuvraj-parashar/",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/yuvraj-parashar/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/yuvraj-parashar",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"https://twitter.com/yuvrajparashar_",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a second year student at The Institute of Technology and Management, Gwalior and pursuing my B.Tech from here. I am a self taught Web Developer. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"https://github.com/yuvraj-parashar",
+    "education":[
+      {
+        "UniversityName":"Institute of Technology and Management",
+        "specialization":"BTech",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2023",
+        "Achievements":"8.3 CGPA"
+      },
+      // {
+      //   "UniversityName":"Some University",
+      //   "specialization":"Some specialization",
+      //   "MonthOfPassing":"Jan",
+      //   "YearOfPassing":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
+    ],
+    "work":[
+      {
+        "CompanyName":"ITM",
+        "specialization":"Front End Web Development",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2019",
+        // "Achievements":"Some Achievements"
+      },
+      // {
+      //   "CompanyName":"Some Company",
+      //   "specialization":"Some specialization",
+      //   "MonthOfLeaving":"Jan",
+      //   "YearOfLeaving":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
+    ],
+    // "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Git and GitHub"
+      },
+      {
+        "skillname":"Django"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"rock-paper-scissors",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
